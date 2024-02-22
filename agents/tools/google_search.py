8352702698d80fd2c5e71891b4659515de385b8d
Mirko Kämpf <mirko.kaempf@gmail.com>
2024-02-22 15:57:08 +0100

    added some documents to our collection2

--- agents/tools/google_search.py
+++
@@ -1,8 +0,0 @@
-from langchain.tools import tool
-from langchain_community.utilities import SerpAPIWrapper
-
-
-@tool
-def google_search(query: str):
-    """Performs a Google search using the provided query string. Choose this tool when you need to find current data"""
-    return SerpAPIWrapper().run(query)
---
+++ demo1/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source ~/.zsh
+
+########################################################################################
+#  This script starts the project's Flask application on the host it is started on.
+#
+
+. /Users/mkaempf/opt/anaconda3/bin/activate && conda activate /Users/mkaempf/opt/anaconda3/envs/ofcc;
+
+# pip3 install -r requirements.txt
+
+streamlit run chatbot_ui.py
